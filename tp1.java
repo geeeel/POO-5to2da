377e37a4a6b65a142d628116242b2b2480249301
geeeel <130399415+geeeel@users.noreply.github.com>
2024-09-16 17:46:59 -0300

    Tp1 Java

---
+++ tp1.java
@@ -0,0 +1,13 @@
+
+public class tp1 {
+	public static void main(String [] args) {
+		for (int i = 0; i < 101 ; i++ ) {
+			if ( i % 2 == 0) {
+				System.out.println (i +" el numero es par");
+			}
+			else {
+				System.out.println (i +"");
+			}
+		}
+	}
+}
